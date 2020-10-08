59fe71be5e1cd60e1d29e333be176bc18455169b
LordBedford <acater@gmail.com>
2020-10-08 12:24:14 -0400

    Fixed incorrect Intrest caclulation

--- src/MoneyMarket.java
+++ src/MoneyMarket.java
@@ -14,7 +14,7 @@ public class MoneyMarket extends Account {
 	@Override
 	public double monthlyInterest() {
 
-		return this.getBalance()*1.0065;
+		return this.getBalance()*0.0065;
 	}
 
 	@Override
